2e8cc52aab4514cbf7d741312fa50d5fba20b5f9
Jovana Jovanoska <jovana.jovanoska003@gmail.com>
2025-04-17 14:11:40 +0200

    Add User model, Role enum, and DTOs for login and register

---
+++ back-end/GeoGenius/src/main/java/mk/ukim/finki/geogenius/model/User.java
@@ -0,0 +1,60 @@
+package mk.ukim.finki.geogenius.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import mk.ukim.finki.geogenius.model.enumerations.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+
+@Entity
+@Table(name = "users")
+@Data
+public class User  implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(role);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ back-end/GeoGenius/src/main/java/mk/ukim/finki/geogenius/model/dto/LoginDto.java
@@ -0,0 +1,9 @@
+package mk.ukim.finki.geogenius.model.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String username;
+    private String password;
+}
